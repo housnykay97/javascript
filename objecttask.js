3d370cca8f574ac2dbf1a61cdd05775253918539
housnykay97 <housnykay97@gmail.com>
2026-05-14 12:08:25 +0300

    task

--- objecttask.js
+++ objecttask.js
@@ -1,19 +1,37 @@
 // 1. Create a new file called objectTask and attempt the below questions:
-// my_arr= [23, “Jane”, 560, [“Lesson”, “Maths”, {“currency” : “KES”}], 987, 76,”John”]
+// my_arr = [23,"Jane", 560, ["Lesson", "Maths", {"currency" : "KES"}], 987, 76, "John"]
+
 // Display:
-// 1. KES
-// 2. 560
-// 3. Maths
+// KES
+let my_arr = [23,"Jane", 560, ["Lesson", "Maths", {"currency" : "KES"}], 987, 76, "John"]
+console.log(my_arr[3][2]["currency"])
+// or console.log(my_arr[3][2].currency)
 
 
+// 560
+console.log(my_arr[2])
 
-// 2. In the object with the key currency, add another key “amount” with value 90
 
+// Maths
+console.log(my_arr[3][1])
+
+
+// 2. In the object with the key currency, add another key "amount" with value 90
+console.log(my_arr[3][2][2] = {"amount" : 90})
+console.log(my_arr)
 
 
 // 3. Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually.
-//      Hint: arrays can be reversed using reverse() functions
+// Hint: arrays can be reversed using reverse() functions
+// my_arr[4]= Number(String(my_arr[4]))
+
+my_arr[4] = Number(String(my_arr[4]).split('').reverse().join(''))
+console.log(my_arr)
 
 
 
 // 4. Change the name “John” to “Jane” . 
+my_arr[6] = "Jane"
+console.log(my_arr)
+
+
